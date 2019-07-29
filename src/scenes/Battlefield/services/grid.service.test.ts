cbd8b041a7644f9204b04d05cbe873e24692fd95
Peter Farabaugh <tapestrygames@outlook.com>
2019-07-29 08:59:48 -0400

    added shortest path to grid service

--- src/scenes/Battlefield/services/grid.service.test.ts
+++ src/scenes/Battlefield/services/grid.service.test.ts
@@ -1,20 +1,99 @@
+import { expect } from "chai";
 import { GridService } from "./grid.service";
 
 describe("grid service", () => {
   describe("toPX", () => {
     it("should convert positions to pixels", () => {
-      expect(GridService.toPx({ x: 0, y: 0 })).toEqual({ x: 0, y: 0 });
-      expect(GridService.toPx({ x: 1, y: 1 })).toEqual({ x: 80, y: 80 });
-      expect(GridService.toPx({ x: 10, y: 5 })).toEqual({ x: 800, y: 400 });
+      expect(GridService.toPx({ x: 0, y: 0 })).to.deep.equal({ x: 0, y: 0 });
+      expect(GridService.toPx({ x: 1, y: 1 })).to.deep.equal({ x: 80, y: 80 });
+      expect(GridService.toPx({ x: 10, y: 5 })).to.deep.equal({
+        x: 800,
+        y: 400
+      });
     });
 
     it("should convert pixels to positions", () => {
-      expect(GridService.toPosition({ x: 0, y: 0 })).toEqual({ x: 0, y: 0 });
-      expect(GridService.toPosition({ x: 1, y: 1 })).toEqual({ x: 0, y: 0 });
-      expect(GridService.toPosition({ x: 780, y: 420 })).toEqual({
+      expect(GridService.toPosition({ x: 0, y: 0 })).to.deep.equal({
+        x: 0,
+        y: 0
+      });
+      expect(GridService.toPosition({ x: 1, y: 1 })).to.deep.equal({
+        x: 0,
+        y: 0
+      });
+      expect(GridService.toPosition({ x: 780, y: 420 })).to.deep.equal({
         x: 9,
         y: 5
       });
+    });
+  });
+  describe("shortest path", () => {
+    let matrix: number[][] = [];
+
+    beforeEach(() => {
+      matrix = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+      ];
+    });
+
+    it("should calculate straight line for same row, no obstacles", () => {
+      const res = GridService.pathBetween(
+        matrix,
+        { x: 1, y: 0 },
+        { x: 4, y: 0 }
+      );
+      expect(res).to.deep.equal([
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 3, y: 0 },
+        { x: 4, y: 0 }
+      ]);
+    });
+    it("should calculate path with diagonals ", () => {
+      const res = GridService.pathBetween(
+        matrix,
+        { x: 1, y: 0 },
+        { x: 4, y: 4 }
+      );
+      expect(res).to.deep.equal([
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 2 },
+        { x: 3, y: 3 },
+        { x: 4, y: 4 }
+      ]);
+    });
+    it("should calculate straight line for same row, with obstacles", () => {
+      matrix[0][3] = 1;
+      const res = GridService.pathBetween(
+        matrix,
+        { x: 1, y: 0 },
+        { x: 4, y: 0 }
+      );
+      expect(res).to.deep.equal([
+        { x: 1, y: 0 },
+        { x: 2, y: 1 },
+        { x: 3, y: 1 },
+        { x: 4, y: 1 },
+        { x: 4, y: 0 }
+      ]);
+    });
+    it("should fail to calculate if blocked", () => {
+      matrix[0][3] = 1;
+      matrix[1][3] = 1;
+      matrix[2][3] = 1;
+      matrix[3][3] = 1;
+      matrix[4][3] = 1;
+      const res = GridService.pathBetween(
+        matrix,
+        { x: 1, y: 0 },
+        { x: 4, y: 0 }
+      );
+      expect(res).to.deep.equal([]);
     });
   });
 });
--- src/scenes/Battlefield/services/grid.service.ts
+++ src/scenes/Battlefield/services/grid.service.ts
@@ -1,4 +1,11 @@
+import PF, { DiagonalMovement } from "pathfinding";
 import { Coord, Position } from "../../../shared/types/coord";
+
+export interface PathOptions {
+  allowDiagonal?: boolean;
+  dontCrossCorners?: boolean;
+  compress?: boolean;
+}
 
 export class GridServiceClass {
   private readonly gridSize: number = 80;
@@ -12,6 +19,29 @@ export class GridServiceClass {
       x: Math.trunc(coord.x / this.gridSize),
       y: Math.trunc(coord.y / this.gridSize)
     };
+  }
+
+  public pathBetween(
+    matrix: number[][],
+    from: Position,
+    to: Position,
+    options: PathOptions = {}
+  ): Position[] {
+    const grid = new PF.Grid(matrix);
+    const pfind = new PF.AStarFinder({
+      diagonalMovement:
+        options.allowDiagonal === undefined || options.allowDiagonal
+          ? options.dontCrossCorners === undefined || options.dontCrossCorners
+            ? DiagonalMovement.OnlyWhenNoObstacles
+            : DiagonalMovement.Always
+          : DiagonalMovement.Never
+    });
+    let path = pfind.findPath(from.x, from.y, to.x, to.y, grid);
+
+    if (options.compress) {
+      path = PF.Util.compressPath(path);
+    }
+    return path.map((r: number[]) => ({ x: r[0], y: r[1] }));
   }
 }
 
