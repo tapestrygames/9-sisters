6d5e51294dab9932c26d3fd685b71eee20699104
Peter Farabaugh <tapestrygames@outlook.com>
2019-07-30 10:24:27 -0400

    set proper starting positions

--- src/scenes/Battlefield/types/GridPosition.ts
+++ src/scenes/Battlefield/types/GridPosition.ts
@@ -1,3 +1,6 @@
+import { Position } from "../../../shared/types/coord";
+
 export interface GridPosition {
   open: boolean;
+  position: Position;
 }
--- src/scenes/Battlefield/types/combatant.ts
+++ src/scenes/Battlefield/types/combatant.ts
@@ -1,9 +1,12 @@
 import { Position } from "../../../shared/types/coord";
 
+export type PositionRuleFunc = (position: Position) => boolean;
+
 export interface Combatant {
   name: string;
   position: Position;
   color: string;
   selected?: boolean;
   movement: number;
+  startingPositionRule?: PositionRuleFunc;
 }
